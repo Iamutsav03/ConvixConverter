530577339f1011d604c2fd186c560c1a302ee083
Bhanu Pratap Singh <nikrcnkhil@gmail.com>
2025-01-09 14:29:12 +0530

    Removal

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Home from './pages/Home'
-import {Routes,Route} from  "react-router-dom"
+import { Route, Routes } from 'react-router-dom'
+
 
 function App() {
   return (
-    <div>
+    <div style={{
+      
+    }} className=''>
       <Routes>
         <Route path="/" element = {<Home/>}/>
       </Routes>
